0b32d3a8ebc66eb208e523195225a4b52d2dff3e
Angelo Cosentino <a.cosentino@teamsystem.com>
2024-09-23 17:59:42 +0200

    changed number of total entries

--- projects/tenant_attributes/populate.sql
+++ projects/tenant_attributes/populate.sql
@@ -2,7 +2,7 @@ WITH
   -- SEED CONFIGURATION AND STATIC DATA
   -- (we use this as source for dictionary-based randomic selections)
   "seed_config"("doc") AS ( VALUES ('{
-    "entries_tot": 1000000,
+    "entries_tot": 50,
     "tenants_tot": 5,   
     "null_values_in_array_possibilities": 15, 
     "countries": ["it","us","fr","es","de"],        
