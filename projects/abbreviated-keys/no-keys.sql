96377b37a24793cd935042ab75a02ce6530d796f
Angelo Cosentino <64754806+angelocosvirgilio@users.noreply.github.com>
2025-05-29 06:31:45 +0000

    rimosso il progetto abbreviato: eliminati file Makefile e script SQL

--- projects/abbreviated-keys/no-keys.sql
+++
@@ -1,5 +0,0 @@
-SET enable_abbrev = off;
-
-EXPLAIN ANALYZE 
-SELECT * FROM "abbrkeys".test_strings 
-ORDER BY text_col;
--- projects/abbreviated-keys/script.sql
+++
@@ -1,6 +0,0 @@
-SET client_min_messages = log;
-SET log_executor_stats = on;
-
-EXPLAIN ANALYZE 
-SELECT * FROM "abbrkeys".test_strings 
-ORDER BY text_col;
